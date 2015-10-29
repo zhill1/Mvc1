560835508f8feb19ca973b4e50de831c44e1d468
zachplusespresso <hill.zachery@gmail.com>
2015-10-29 12:54:53 -0500

    Initial Setup

---
+++ Mvc1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc1.Startup))]
+namespace Mvc1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
